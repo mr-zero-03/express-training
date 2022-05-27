16ff8f28d1a9a4949467f3f6c07e6c02ab0bd8dd
mr-zero-03 <danielsantiagoperez03@gmail.com>
2022-05-27 14:23:46 -0500

    Added another 2 endpoints   The response of one of them is a JSON

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ app.get( '/', ( req, res ) => {
   res.send( 'Hello World from Express!' );
 } );
 
+app.get( '/second-endpoint', ( req, res ) => {
+  res.send( 'Welcome to the 2nd Endpoint!' );
+} );
+
+app.get( '/product/tshirt', ( req, res ) => {
+  const product = {
+    name: 'T-shirt',
+    price: 2000,
+    color: 'red'
+  }
+
+  res.json( product ); //Response with JSON
+} );
+
+//---
 app.listen( port, host, () => {
   console.log( 'Listening on the port: "' + port + '"' );
 } );
