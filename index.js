780f95b15044d0824df17c323349f2f227ab0b13
mr-zero-03 <danielsantiagoperez03@gmail.com>
2022-09-20 14:27:32 -0500

    Added the cors library

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const cors = require( 'cors' );
 const app = express(); //appRouter - expressRouter - router
 const apiRoutes = require( './routes' );
 
@@ -10,6 +11,24 @@ const errorHandler = require( './middlewares/error_handler.js' ).errorHandler;
 const host = 'localhost';
 const port = 3000;
 //--
+
+const whiteList = [
+  'http://localhost',
+  'https://api.mydomain.com'
+];
+
+const options = {
+  origin: ( origin, callback ) => {
+    if ( whiteList.includes( origin ) ) {
+      callback( null, true );
+    } else {
+      callback( new Error( 'Not Allowed' ) );
+    }
+  }
+};
+
+//app.use( cors( options ) );
+app.use( cors() );
 
 app.get( '/', ( req, res ) => {
   res.send( 'Welcome World to my Tiendita from Express!' );
